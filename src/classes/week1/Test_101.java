a574ef639bf6eb8efbf5e783855d6fb09c2daa0c
boazben-moshe <benmo@simhawk.tech>
2024-10-18 17:42:07 +0300

    init Commit classes 1-6

---
+++ src/classes/week1/Test_101.java
@@ -0,0 +1,18 @@
+package classes.week1;
+/*
+ * output:
+Hello World!
+this is a nice String 15.899999999999999
+ */
+public class Test_101 {
+	public static void main(String[] args) {
+		double i = 3;
+		i = i *5.3;
+		System.out.print("Hello World!");
+	//	System.out.println();
+		String msg = "this is a nice String ";
+		int d = 4;
+		System.out.println(msg + d);
+	}
+
+}
