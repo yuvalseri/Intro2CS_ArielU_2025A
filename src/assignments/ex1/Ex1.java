bc9f80879fc8cdd04877d6a40ee173e15737ee0c
yuval <yuvalseri10@gmail.com>
2024-12-10 20:27:44 +0200

    ניסיון

--- src/assignments/ex1/Ex1.java
+++ src/assignments/ex1/Ex1.java
@@ -19,36 +19,38 @@ public class Ex1 {
          * @return
          */
         public static int number2Int(String num) {
+
+            ////////////////////
             if (num == null || num.isEmpty()) {
+                return -1; // the num is invalid
+            }
+            if(!isNumber(num)){
                 return -1;
             }
             int bindex = num.indexOf('b');
-            if (bindex == 0 || bindex == num.length() - 1) {
-                return -1;
-            }
+            int base = 10; // Difing the defult base as 10
 
-            String numPart = num.substring(0, bindex);
-            String basePart = num.substring(bindex + 1, num.length() - 1);
-            char baseChar = basePart.charAt(0);
-            int base = Character.digit(baseChar, 36); //Converting the char to int value
-
-            if (!isValidbase(basePart)) {
-                return -1;
+            if (bindex != -1) {
+                if (bindex == 0 || bindex == num.length() - 1) {
+                    return -1;
+                }
+                String basePart = num.substring(bindex + 1);
+                base = Character.digit(basePart.charAt(0), 36); // Converting the char to numerical value
+                if (base < 2 || base > 16) { // if base is not between 2 to 16
+                    return -1; // the base is invalid
+                }
+                num = num.substring(0, bindex); // החלק של המספר עצמו
             }
-            // add your code here
+            // בדיקה של הספרות בבסיס הנבחר
             int decimalnum = 0;
-            int power = 0;
-            char[] chars = numPart.toCharArray();
-
-            for (int i = chars.length-1; i >= 0; i--) {
-                char c = chars[i];
-                int digit = Character.digit(c, base);
-                decimalnum += (int) (digit * (Math.pow(base,power)));
-                power += 1;
+            for (int i = 0; i < num.length(); i++) {
+                int digit = Character.digit(num.charAt(i), base);
+                if (digit == -1) {
+                    return -1; //
+                }
+                decimalnum = decimalnum * base + digit;
             }
             return decimalnum;
-            ////////////////////
-
         }
         public static boolean isValidbase(String basePart){
             if (basePart.length() != 1){                     //if the length of the base is different from 1
@@ -85,62 +87,35 @@ public class Ex1 {
          * @return true iff the given String is in a number format
          */
         public static boolean isNumber(String a) {
-            boolean ans = true;
-            // add your code here
-            if (a == null || a.isEmpty()) {
+            boolean ans= true;
+            String validChar= "0123456789ABCDEFG";
+            String num= "0123456789";
+            char lastChar = a.charAt(a.length()-1);
+            if (a.length()<2 && num.contains(String.valueOf(lastChar))){
+                return true;
+            }
+            else if(a.length()<2){
+                return false;}
+            char secondTolast =a.charAt(a.length()-2);
+            char firstChar =a.charAt(0);
+            if(firstChar=='b' || firstChar == ' '){
                 return false;
             }
-            int bindex = a.indexOf('b');
-            if (bindex == 0 || bindex == a.length()-1) {
-                return false;
-            }
-            String numPart = a.substring(0, bindex);
-            String basePart = a.substring(bindex + 1);
-
-            if (!isValidnum(numPart)) {
-                return false;
-            }
-            if (!isValidbase(basePart)) {
-                return false;
-            }
-
-            char baseChar = basePart.charAt(0);
-            int base = Character.digit(baseChar, 36);
-            if(base<2 || base>16){
-                return false;
+            if (!validChar.contains(String.valueOf(lastChar)) && secondTolast !='b'){
+                return false;}
+            int baseIndex =validChar.indexOf(lastChar);
+            int indexOf2 = a.length()-2;
+            String currentString =validChar.substring(0, baseIndex);
+            String numPart= a.substring(0,indexOf2);
+            for (int i=0; i< numPart.length(); i++){
+                char currentChar = a.charAt(i);
+                if (!currentString.contains(String.valueOf(currentChar))){
+                    ans = false;
+                }
             }
             return ans;
-
-
         }
 
-    private static boolean isValidBase(String basePart) {
-        if (basePart.isEmpty()) {
-            return false;
-        }
-
-        for (char c : basePart.toCharArray()) {
-            if (!Character.isDigit(c)) {
-                return false; // Base must only contain digits
-            }
-        }
-
-        int base = Integer.parseInt(basePart); // Safe to parse now
-        return base >= 2 && base <= 16;
-    }
-
-    private static boolean isValidNumForBase(String numPart, int base) {
-        if (numPart.isEmpty()) {
-            return false; // No digits
-        }
-
-        for (char c : numPart.toCharArray()) {
-            if (Character.digit(c, base) == -1) {
-                return false; // Invalid digit for the base
-            }
-        }
-        return true;
-    }
 
         /**
          * Calculate the number representation (in basis base)
@@ -178,11 +153,11 @@ public class Ex1 {
          * @return true iff the two numbers have the same values.
          */
         public static boolean equals(String n1, String n2) {
-            boolean ans = true;
-         if(number2Int(n1) != number2Int(n2)){
+            boolean ans = true; //Initializing ans as true
+         if(number2Int(n1) != number2Int(n2)){ //if the numerical value of n1 is different from the value of n2
              return false;
             }
-         return ans;
+         return ans; // return true;
         }
 
         /**
@@ -194,10 +169,16 @@ public class Ex1 {
          *
          */
         public static int maxIndex(String[] arr) {
-            int ans = 0;
-            // add your code here
+            int maxIndex = -1; // Initialize the max index as -1
+            int maxValue = Integer.MIN_VALUE; //Initialize the max value as the min value of Java
 
-            ////////////////////
-            return ans;
+        for(int i=0; i< arr.length; i++){ //A loop passes all the index in the array
+            int currentValue = number2Int(arr[i]); //Difining the currentValue to the numerical value of arr[i]
+            if(currentValue != -1 && currentValue > maxValue){ // if the current value is valid and the current value greater than the maxValue
+                maxValue= currentValue; // Update the maxValue to the currentValue;
+                maxIndex=i ; // Update the maxIndex to i
+            }
+        }
+        return maxIndex; // return the maxIndex
         }
 }
